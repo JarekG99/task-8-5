dacc98825548f5b65334f4b78746113d0922479b
Jarek Gajdulewicz <j.gajdulewicz@icloud.com>
2018-01-25 15:42:02 +0100

    Add script for triangle area

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// scripts.js
+
+var h = prompt('Enter h - triangle height');
+var a = prompt('Enter a - triangle base')
+var triangleArea = a*h/2
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+alert('The triangle field is:' + triangleArea );
